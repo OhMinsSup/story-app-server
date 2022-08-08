f8ddee3aace35477385223a879f5b332d3b08e27
OhMinSeop <mins5190@gmail.com>
2022-08-08 21:52:07 +0900

    feat: main.ts swagger, default module setting and prisma connection

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+import type { INestApplication } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        { emit: 'stdout', level: 'query' },
+        { emit: 'stdout', level: 'error' },
+      ],
+      errorFormat: 'pretty',
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import compression from 'compression';
+
+import { PrismaService } from './database/prisma.service';
+
 import { AppModule } from './app.module';
 
+import type { NestExpressApplication } from '@nestjs/platform-express';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const prisma: PrismaService = app.get(PrismaService);
+  prisma.enableShutdownHooks(app);
+
+  const config = new DocumentBuilder()
+    .setTitle('Story API Server')
+    .setDescription('Story NFT Market App API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addCookieAuth('access_token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.use(helmet());
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.use(compression());
+
   await app.listen(3000);
 }
 bootstrap();
